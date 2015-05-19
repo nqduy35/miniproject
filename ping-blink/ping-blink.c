c1ff6ff78fccceed1905d5394d7b1ccd1123acbe
nqduy35 <nqduy35@gmail.com>
2015-05-19 16:59:36 +0200

    1659190515

--- ping-blink/ping-blink.c
+++ ping-blink/ping-blink.c
@@ -17,13 +17,12 @@
 // Global definition
 /*--------------------------------------------------------------------------------*/
 #define OUT_PORT 4444
-#define IN_POUT 1111
+#define IN_PORT 1111
+#define MAX_PAYLOAD_LEN 30
 
-static struct uip_udp_conn *client_conn;
-static struct uip_udp_conn *server_conn;
-static uip_ipaddr_t client_ipaddr;
-static uip_ipaddr_t server_ipaddr;
-
+static struct uip_udp_conn *connection;
+static uip_ipaddr_t destination_ipaddr;
+static uip_ipaddr_t source_ipaddr;
 
 // Starting of contiki program
 /*--------------------------------------------------------------------------------*/
@@ -43,23 +42,33 @@ static void set_global_address() {
     uip_ds6_addr_add(&ipaddr, 0, ADDR_AUTOCONF);
     
     #if 0
-	uip_ip6addr(&server_ipaddr, 0xaaaa, 0, 0, 0, 0, 0, 0, 1);
+	uip_ip6addr(&source_ipaddr, 0xaaaa, 0, 0, 0, 0, 0, 0, 1);
     #elif 1
-	uip_ip6addr(&server_ipaddr, 0xaaaa, 0, 0, 0, 0, 0x00ff, 0xfe00, 1);
+	uip_ip6addr(&source_ipaddr, 0xaaaa, 0, 0, 0, 0, 0x00ff, 0xfe00, 1);
     #else
-	uip_ip6addr(&server_ipaddr, 0xaaaa, 0, 0, 0, 0x0250, 0xc2ff, 0xfea8, 0xcd1a);
+	uip_ip6addr(&source_ipaddr, 0xaaaa, 0, 0, 0, 0x0250, 0xc2ff, 0xfea8, 0xcd1a);
     #endif
 }
+
+static void send_packet() {
+    static int seq_id;
+    char buf[MAX_PAYLOAD_LEN];
+
+    seq_id++;
+    printf("ping %s", destination_ipaddr);
+    uip_udp_packet_sendto(connection, buf, strlen(buf), &destination_ipaddr, UIP_HTONS(IN_PORT));
+}
+
 
 // Process thread
 /*--------------------------------------------------------------------------------*/
 PROCESS_THREAD(ping_process, ev, data) {
     PROCESS_BEGIN();
 
-    set_global_address();
-
     SENSORS_ACTIVATE(button_sensor);
 
+    set_global_address();
+    
     while(1) {
 	PROCESS_WAIT_EVENT_UNTIL(ev == sensors_event && data == &button_sensor);
 
@@ -69,6 +78,13 @@ PROCESS_THREAD(ping_process, ev, data) {
 	leds_off(0xFF);
 
 	// send data
+	connection = udp_new(NULL, UIP_HTONS(OUT_PORT), NULL);
+        if(connection == NULL) {
+            printf("No connection");
+            PROCESS_EXIT();
+        }
+        udp_bind(connection, UIP_HTONS(IN_PORT));
+	send_packet();
     }
     PROCESS_END();
 }
