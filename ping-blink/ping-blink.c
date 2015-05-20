af260000296755949bdd71724eb47e31edbb0787
nqduy35 <nqduy35@gmail.com>
2015-05-20 16:54:10 +0200

    1653190515

--- ping-blink/ping-blink.c
+++ ping-blink/ping-blink.c
@@ -8,6 +8,8 @@
 #include "net/uip-udp-packet.h"
 #include "net/rpl/rpl.h"
 #include "net/netstack.h"
+#include "simple-udp.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -16,12 +18,12 @@
 
 // Global definition
 /*--------------------------------------------------------------------------------*/
-#define OUT_PORT 4444
-#define IN_PORT 1111
+#define DES_PORT 1234
+#define SRC_PORT 1234
 #define MAX_PAYLOAD_LEN 30
 
-static struct uip_udp_conn *connection;
-static uip_ipaddr_t destination_ipaddr;
+static struct simple_udp_conn *connection;
+static uip_ipaddr_t destin_ipaddr;
 static uip_ipaddr_t source_ipaddr;
 
 // Starting of contiki program
@@ -35,28 +37,27 @@ AUTOSTART_PROCESSES(&ping_process, &blink_process);
 // Global functions
 /*--------------------------------------------------------------------------------*/
 static void set_global_address() {
-    uip_ipaddr_t ipaddr;
-    
-    uip_ip6addr(&ipaddr, 0xaaaa, 0, 0, 0, 0, 0, 0, 0);
-    uip_ds6_set_addr_iid(&ipaddr, &uip_lladdr);
-    uip_ds6_addr_add(&ipaddr, 0, ADDR_AUTOCONF);
-    
-    #if 0
-	uip_ip6addr(&source_ipaddr, 0xaaaa, 0, 0, 0, 0, 0, 0, 1);
-    #elif 1
-	uip_ip6addr(&source_ipaddr, 0xaaaa, 0, 0, 0, 0, 0x00ff, 0xfe00, 1);
-    #else
-	uip_ip6addr(&source_ipaddr, 0xaaaa, 0, 0, 0, 0x0250, 0xc2ff, 0xfea8, 0xcd1a);
-    #endif
-}
+    // Source address
+    uip_ip6addr(&source_ipaddr, 0xaaaa, 0, 0, 0, 0, 0, 0, 0);
+    uip_ds6_set_addr_iid(&source_ipaddr, &uip_lladdr);
+    uip_ds6_addr_add(&source_ipaddr, 0, ADDR_AUTOCONF);    
 
+    // Destination address
+    uip_ip6addr(&destin_ipaddr, 0xaaaa, 0, 0, 0, 0x0212, 0x7402, 0x0002, 0x0202);
+}
 static void send_packet() {
     static int seq_id;
     char buf[MAX_PAYLOAD_LEN];
 
     seq_id++;
-    printf("ping %s", destination_ipaddr);
-    uip_udp_packet_sendto(connection, buf, strlen(buf), &destination_ipaddr, UIP_HTONS(IN_PORT));
+    printf("ping %s", destin_ipaddr);
+    //uip_udp_packet_sendto(connection, buf, strlen(buf), &destin_ipaddr, UIP_HTONS(IN_PORT));
+}
+static void receiver(struct simple_udp_connection *c, const uip_ipaddr_t *sender_addr,
+    uint16_t sender_port, const uip_ipaddr_t *receiver_addr, uint16_t receiver_port,
+    const uint8_t *data, uint16_t datalen) {
+    // Callback
+    printf("Callback");
 }
 
 
@@ -68,7 +69,10 @@ PROCESS_THREAD(ping_process, ev, data) {
     SENSORS_ACTIVATE(button_sensor);
 
     set_global_address();
-    
+
+    // remote IP equal NULL -> packet from anywhere
+    simple_udp_register(&connection, SRC_PORT, &destin_ipaddr, DES_PORT, receiver);
+
     while(1) {
 	PROCESS_WAIT_EVENT_UNTIL(ev == sensors_event && data == &button_sensor);
 
@@ -78,13 +82,16 @@ PROCESS_THREAD(ping_process, ev, data) {
 	leds_off(0xFF);
 
 	// send data
-	connection = udp_new(NULL, UIP_HTONS(OUT_PORT), NULL);
-        if(connection == NULL) {
-            printf("No connection");
-            PROCESS_EXIT();
-        }
-        udp_bind(connection, UIP_HTONS(IN_PORT));
-	send_packet();
+	char buf[20] = "Ohaio!!!";
+	simple_udp_sendto(&connection, buf, strlen(buf) + 1, &destin_ipaddr);
+
+	//connection = udp_new(NULL, UIP_HTONS(OUT_PORT), NULL);
+        //if(connection == NULL) {
+        //    printf("No connection");
+        //    PROCESS_EXIT();
+        //}
+        //udp_bind(connection, UIP_HTONS(IN_PORT));
+	//send_packet();
     }
     PROCESS_END();
 }
